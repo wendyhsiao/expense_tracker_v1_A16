7ca848ffb9e306b184a9f09e037038d75e573867
wendy.hsiao <wendy19940331@gmail.com>
2019-09-18 00:44:03 +0800

    Add Mongoose connection and create Record and User model

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
+// 設定 mongoDB
+mongoose.connect('mongodb://localhost/expenseTracker', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
+// 載入 model
+const Record = require('./models/record.js')
+const User = require('./models/user.js')
+
+// 設定路由
 app.use('/', require('./routes/home.js'))
 app.use('/expenseTracker', require('./routes/expenseTracker.js'))
 
---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userdSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('User', userdSchema)
