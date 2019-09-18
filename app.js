018f81e17a8f76d019d65e5accf83af34b2dc03e
wendy.hsiao <wendy19940331@gmail.com>
2019-09-19 00:09:51 +0800

    Add new page and create feature, install body-parser

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // 設定 mongoDB
 mongoose.connect('mongodb://localhost/expenseTracker', {
--- routes/expenseTracker.js
+++ routes/expenseTracker.js
@@ -2,23 +2,35 @@ const express = require('express')
 const router = express.Router()
 
 // 載入 model
-// const Record = require('./models/record.js')
+const Record = require('../models/record.js')
 // const User = require('./models/user.js')
 
 router.get('/', (req, res) => {
-  res.redirect('/')
+  return res.redirect('/')
+})
+
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const newRecord = new Record({
+    name: req.body.name,
+    category: req.body.category,
+    date: req.body.date,
+    amount: req.body.amount
+  })
+
+  newRecord
+    .save()
+    .then(item => {
+      return res.redirect('/')
+    })
+    .catch(err => console.log(err))
 })
 
 router.get('/:id', (req, res) => {
   res.send('詳細頁面')
-})
-
-router.get('/new', (req, res) => {
-  res.send('新增頁面')
-})
-
-router.post('/', (req, res) => {
-  res.send('新增頁面')
 })
 
 router.get('/:id/edit', (req, res) => {
