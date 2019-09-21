8aa06733940b8f03521e2d04ded7e9999cde15f2
wendy.hsiao <wendy19940331@gmail.com>
2019-09-21 19:19:28 +0800

    Add user router, and login and register templates

---
+++ routes/user.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', (req, res) => {
+  res.render('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  res.render('register')
+})
+
+router.get('/logout', (req, res) => {
+  res.render('logout')
+})
+
+module.exports = router
