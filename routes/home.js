35ba1de9f75c8ccf3a507ca606c41ddfaa4ba6fe
wendy.hsiao <wendy19940331@gmail.com>
2019-09-22 22:35:54 +0800

    Add icon to index page

--- routes/home.js
+++ routes/home.js
@@ -4,50 +4,49 @@ const Handlebars = require('handlebars')
 
 // 載入 model
 const Record = require('../models/record.js')
-// const User = require('./models/user.js')
+
 const { authenticated } = require('../config/auth')
 
+Handlebars.registerHelper('select', function(selected, options) {
+  return options
+    .fn(this)
+    .replace(
+      'value="/?category=' + selected + '"',
+      'value="/?category=' + selected + '" selected="selected"'
+    )
+})
+
+// icon選擇
+Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+  if (arg1 == arg2) {
+    return options.fn(this)
+  } else {
+    return options.inverse(this)
+  }
+})
+
 router.get('/', authenticated, (req, res) => {
-  const category = req.query.category
-  Handlebars.registerHelper('ifhome', function(arg1, arg2, arg3, options) {
-    if (arg1 == arg2 || arg3 == arg2) {
-      return options.fn(this)
-    } else {
-      return options.inverse(this)
-    }
+  let category = req.query.category
+  let filterObject = { userId: req.user._id }
+  if (req.query.category) {
+    filterObject.category = category
+  }
+
+  Record.find(filterObject).exec((err, records) => {
+    console.log('records', records)
+    if (err) return console.error(err)
+    let totalAmount = 0
+    records.forEach(recordsOne => {
+      totalAmount += recordsOne.amount
+    })
+
+    return res.render('index', {
+      records: records,
+      item: records[0],
+      itemAll: '類別 / 全部',
+      totalAmount
+    })
   })
-
-  category
-    ? Record.find({ userId: req.user._id, category })
-        .sort({})
-        .exec((err, records) => {
-          if (err) return console.error(err)
-          let sum = 0
-          records.forEach(recordsOne => {
-            sum += recordsOne.amount
-          })
-
-          return res.render('index', {
-            records: records,
-            item: records[0],
-            sum
-          })
-        })
-    : Record.find({ userId: req.user._id })
-        .sort({})
-        .exec((err, records) => {
-          if (err) return console.error(err)
-          let sum = 0
-          records.forEach(recordsOne => {
-            sum += recordsOne.amount
-          })
-
-          return res.render('index', {
-            records: records,
-            itemAll: '類別 / 全部',
-            sum
-          })
-        })
 })
 
 module.exports = router
