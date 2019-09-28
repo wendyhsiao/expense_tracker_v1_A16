b6a4c192aa81edd84d4f70d7df9fd02ae07f8391
wendy.hsiao <wendy19940331@gmail.com>
2019-09-28 15:44:56 +0800

    Update Icon display method

--- routes/home.js
+++ routes/home.js
@@ -17,13 +17,21 @@ Handlebars.registerHelper('select', function(selected, options) {
 })
 
 // icon選擇
-Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
-  if (arg1 == arg2) {
-    return options.fn(this)
-  } else {
-    return options.inverse(this)
-  }
-})
+// Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+//   if (arg1 == arg2) {
+//     return options.fn(this)
+//   } else {
+//     return options.inverse(this)
+//   }
+// })
+
+const categoryIcon = {
+  houseware: `<i class="fas fa-home fa-2x" id="fa-home"></i>`,
+  traffic: `<i class="fas fa-shuttle-van fa-2x" id="fa-shuttle-van"></i>`,
+  entertainment: `<i class="fas fa-grin-beam fa-2x" id="fa-grin-beam"></i>`,
+  food: `<i class="fas fa-utensils fa-2x" id="fa-utensils"></i>`,
+  other: `<i class="fas fa-pen fa-2x" id="fa-pen"></i>`
+}
 
 router.get('/', authenticated, (req, res) => {
   let category = req.query.category
@@ -31,13 +39,16 @@ router.get('/', authenticated, (req, res) => {
   if (req.query.category) {
     filterObject.category = category
   }
-
+  console.log('category1', category)
   Record.find(filterObject).exec((err, records) => {
     console.log('records', records)
     if (err) return console.error(err)
     let totalAmount = 0
     records.forEach(recordsOne => {
       totalAmount += recordsOne.amount
+      recordsOne.icon = categoryIcon[recordsOne.category]
+      console.log('category', category)
+      console.log('recordsOne.icon', recordsOne.icon)
     })
 
     return res.render('index', {
